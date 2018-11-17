9f66d82b95c44322fefaf93f0912e69190b74fdd
ramya <ramya@askpundit.com>
2018-11-17 18:47:31 +0530

    Development

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark default-color-dark fixed-top">
+        <a class="navbar-brand" href="index.html" style="color: #333;"><img src="https://vuejs.org/images/logo.png" style="width: 35px;"/>VUE JS</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+          
+          <ul class="navbar-nav ml-auto nav-flex-icons">
+             <li class="nav-item"><a class="nav-link" href="#">Menu 1</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Menu 2</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Menu 3</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Menu 4</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+
+<template>
+  <div class="hello">
+    <table style="width:65%;height:90%;" align="center">    
+      <tr style="background-color:#32A9EE;color:white;">
+        <th>Rank</th>
+        <th>Team</th>
+        <th>Point</th>
+        <th>Rating</th>
+      </tr>
+      <tr v-for="item in items">
+        <td><b>{{item.rank}}</b></td>
+        <td>{{item.team}}</td>
+        <td>{{item.point}}</td>
+        <td>{{item.rating}}</td>
+      </tr>
+    </table>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to VUE JS',
+      items: [
+        {rank: 1,team:'Bangladesh', point: '11',rating:'21'},
+        {rank: 2,team:'India', point: '12',rating:'22'},
+        {rank: 3,team:'West Indies', point: '13',rating:'23'},
+        {rank: 4,team:'Pakistan', point: '14',rating:'24'},
+        {rank: 5,team:'Zimbabwe', point: '15',rating:'25'}
+      ]
+    }
+  }
+  
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
